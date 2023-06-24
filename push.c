0f465424460e343c61104745e524d713b7a31083
Your Name <you@example.com>
2023-06-24 13:38:25 -0700

    monty

--- push.c
+++ push.c
@@ -1,53 +1,135 @@
 #include "monty.h"
 
 /**
- * push - pushes an element to the stack.
- * @stack: the stack
- * @line_number: the current line number
+ * push - Add node to the stack
+ * @stack: head of linkedlist
+ * @line_number: line number of the instruction
  *
- * Return: void
+ * Return: No return
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *new, *tmp;
-	char *push_arg = strtok(NULL, "\n \t");
-	int pVal;
-	/*if push, tests if the push_arg was valid or not */
-	if (!is_int(push_arg))
-	{
-		fprintf(stdout, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
 
+	int n = 0;
 
-	pVal = atoi(push_arg);
-	new = malloc(sizeof(stack_t));
-	if (new == NULL)
+	if (globalvar.token2 == NULL)
 	{
-		fprintf(stdout, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
+		free_dlistint(*stack);
+		stderr_int(line_number);
 	}
-	new->n = pVal;
-	new->prev = NULL;
-	new->next = NULL;
-	/** checks if stack is empty **/
-	if ((*stack) == NULL)
-		*stack = new;
-	else if (SQ)
+	if (!_isdigit() || stack == NULL)
 	{
-		/** puts new node on top if not empty **/
-		(*stack)->prev = new;
-		new->next = *stack;
-		*stack = new;
+		free_dlistint(*stack);
+		stderr_int(line_number);
+	}
+	n = atoi(globalvar.token2);
+	if (*stack  == NULL)
+	{
+		create_node_stackfirst(stack, n);
 	}
 	else
 	{
-		/**puts new node on the bottom **/
-		tmp = *stack;
-		while (tmp->next != NULL)
-			tmp = tmp->next;
-		tmp->next = new;
-		new->prev = tmp;
+		create_node_stackend(stack, n);
 	}
+}
 
+/**
+ * pall - Print the stack
+ * @stack: head of linkedlist
+ * @line_number: line number of the instruction
+ *
+ * Return: No return
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+
+	stack_t *temp = NULL;
+
+
+	if (*stack == NULL)
+	{
+		return;
+	}
+	if (*stack == NULL && line_number != 1)
+	{
+		free_dlistint(*stack);
+		free_globalvars();
+		exit(EXIT_SUCCESS);
+	}
+	temp = *stack;
+	while (temp->next != NULL)
+		temp = temp->next;
+	while (temp->prev != NULL)
+	{
+		printf("%d", temp->n);
+		temp = temp->prev;
+		printf("\n");
+	}
+	printf("%d\n", temp->n);
+}
+
+
+/**
+ * pint - Print the stack
+ * @stack: head of linkedlist
+ * @line_number: line number of the instruction
+ *
+ * Return: No return
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+
+	stack_t *temp = NULL;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		pint_e(line_number);
+		return;
+	}
+	temp = *stack;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	printf("%d", temp->n);
+	printf("\n");
+}
+
+/**
+ * swap - Print the stack
+ * @stack: head of linkedlist
+ * @line_number: line number of the instruction
+ *
+ * Return: No return
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	int i, j;
+
+	if (*stack == NULL || stack == NULL)
+		op_e(line_number, "swap");
+
+	temp = (*stack)->next;
+	if ((*stack)->next == NULL)
+		op_e(line_number, "swap");
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	i = temp->n;
+	j = temp->prev->n;
+	temp->n = j;
+	temp->prev->n = i;
+}
+/**
+ * nop - does not do anything
+ * @stack: head of linkedlist
+ * @line_number: line number of the instruction
+ *
+ * Return: No return
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
 }
